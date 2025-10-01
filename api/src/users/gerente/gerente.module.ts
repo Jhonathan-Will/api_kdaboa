3934e5fe50447905f167df6dac495d40b7160381
Jhonathan William <jhonathanwilliam400@gmail.com>
2025-09-30 21:00:30 -0300

    Feat(gerente): updating the module for the employee

--- api/src/users/gerente/gerente.module.ts
+++ api/src/users/gerente/gerente.module.ts
@@ -6,9 +6,10 @@ import { CsrfModule } from 'src/security/csrf/csrf.module';
 import { UsersModule } from '../users.module';
 import { UsersService } from '../users.service';
 import { FeatureModule } from 'src/features/features.module';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
-  imports: [CsrfModule, UsersModule, FeatureModule],
+  imports: [CsrfModule, UsersModule, FeatureModule, EmailModule],
   controllers: [GerenteController],
   providers: [PrismaService, GerenteService, UsersService]
 })
