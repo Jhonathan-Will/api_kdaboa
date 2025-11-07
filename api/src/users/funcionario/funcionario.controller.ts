d3f44979ecd8abcee973c8e60d5831e176e377c9
Jhonathan William <jhonathanwilliam400@gmail.com>
2025-11-07 01:07:09 -0300

    Feat(Funcionario): Making the employee put files in the correct way

--- api/src/users/funcionario/funcionario.controller.ts
+++ api/src/users/funcionario/funcionario.controller.ts
@@ -1,12 +1,13 @@
-import { Body, Controller, HttpException, Post, Req, UseGuards, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, HttpException, HttpStatus, Param, ParseIntPipe, Post, Put, Req, Res, UseGuards, UseInterceptors } from "@nestjs/common";
 import { FuncionarioService } from "./funcionario.service";
 import { CsrfService } from "src/security/csrf/csrf.service";
 import { RefreshGuard } from "src/security/jwt/guard/refresh.guard";
 import { ApiBody, ApiConsumes, ApiOperation } from "@nestjs/swagger";
 import { CriarEventoDTO } from "./dto/criarEvento.dto";
 import { FileInterceptor } from "@nestjs/platform-express";
-import { diskStorage } from 'multer';
-import { extname, join } from 'path';
+import { Response } from 'express';
+import { UploadedFile } from '@nestjs/common';
+import { ImageHandlePipe } from "src/common/pipe/imageHandle.pipe";
 
 @Controller('funcionario')
 export class FuncionarioController {
@@ -14,6 +15,7 @@ export class FuncionarioController {
                 private readonly csrf: CsrfService) {}
     
     //rota para cadastrar evento
+    @Post('/event')
     @UseGuards(RefreshGuard)
     @ApiOperation({ summary: 'Cadastra novo evento para validar pelo gerente'})
     @ApiBody({
@@ -68,33 +70,12 @@ export class FuncionarioController {
             ]
         }
     })
-    @UseInterceptors(FileInterceptor('images', {
-        storage: diskStorage({
-            destination: join(__dirname, "..", "..", "images", "events").replace("dist", "src"),
-            filename: (req, file, cb) => {
-                const randomPart = Math.round(Math.random() * 1E12).toString().slice(-10);
-                const uniqueSuffix = req.user.sub + "-" + Date.now() + '-' + randomPart;
-                const extension = extname(file.originalname);
-
-                const fileName = `${uniqueSuffix}`;
-                const filePath = join(__dirname, "..", "..", "images", "events", fileName).replace("dist", "src");
-                const fs = require('fs');
-                if (fs.existsSync(filePath)) {
-                    const newRandomPart = Math.round(Math.random() * 1E12).toString().slice(-10);
-                    const newUniqueSuffix = req.user.sub + "-" + Date.now() + '-' + newRandomPart;
-                    cb(null, `${newUniqueSuffix}${extension}`);
-                } else {
-                    cb(null, `${uniqueSuffix}${extension}`);
-                }
-            }
-        })
-    }))
+    @UseInterceptors(FileInterceptor("images"))
     @ApiConsumes('multipart/form-data')
-    @Post('/event')
-    async CadastraEvento(@Body() evento: CriarEventoDTO, @Req() req: any) {
+    async CadastraEvento(@Body() evento: CriarEventoDTO, @Req() req: any, @UploadedFile(  new ImageHandlePipe('events', true) ) file: Express.Multer.File) {
         if (this.csrf.validateToken(req.cookies['x-csrf-token'] || req.headers['x-csrf-token'])) {
-
-            this.funcionarioService.cadastraEvento(evento, req.user.sub, req.file.filename)
+            
+            this.funcionarioService.cadastraEvento(evento, req.user.sub, file.filename)
 
         } else {
             throw new HttpException({
@@ -103,5 +84,78 @@ export class FuncionarioController {
             }, 405);
         }
     }
+    
+    //rota para alterar evento
+    @Put('/event/:id')
+    @UseGuards(RefreshGuard)
+    @ApiOperation({ summary: 'Altera algum evento' })
+    @ApiBody({
+        schema: {
+            type: 'object',
+            properties: {
+                images: {
+                    type: 'string',
+                    format: 'binary',
+                    description: 'Imagem do evento'
+                },
+                nome: {
+                    type: 'string',
+                    example: 'Festa de Aniversário',
+                    description: 'Nome do evento'
+                },
+                descricao: {
+                    type: 'string',
+                    example: 'Uma festa incrível para celebrar meu aniversário',
+                    description: 'Descrição do evento'
+                },
+                data_inicio: {
+                    type: 'string',
+                    example: '2023-10-15T18:00:00Z',
+                    description: 'Data de início do evento'
+                },
+                data_fim: {
+                    type: 'string',
+                    example: '2023-10-15T23:59:59Z',
+                    description: 'Data de fim do evento'
+                },
+                id_endereco: {
+                    type: 'integer',
+                    example: 1,
+                    description: 'ID do estabelecimento onde o evento será realizado'
+                },
+                categoria: {
+                    type: 'array',
+                    items: { type: 'integer', example: 1 },
+                    example: [1, 2, 3],
+                    description: 'Categoria do evento, representada por um array de números'
+                }
+            },
+            required: [
+                'images',
+                'nome',
+                'descricao',
+                'data_inicio',
+                'data_fim',
+                'id_endereco',
+                'categoria'
+            ]
+        }
+    })
+    @UseInterceptors(FileInterceptor('images'))
+    @ApiConsumes('multipart/form-data')
+    async AltaraEvento(
+        @Param('id', ParseIntPipe) id: number,
+        @UploadedFile( new ImageHandlePipe('events', false)) file: Express.Multer.File,
+        @Body() evento: CriarEventoDTO,
+        @Req() req: any,
+        @Res() res: Response
+    ) {
+        if (!this.csrf.validateToken(req.cookies['x-csrf-token'] || req.headers['x-csrf-token'])) {
+            throw new HttpException({ status: 403, error: 'Token CSRF inválido' }, 405);
+        }
 
+        res.status(HttpStatus.OK).json(
+            await this.funcionarioService.alteraEvento(req.user.sub, id, file.filename, evento)
+        );
+    }
 }
--- api/src/users/funcionario/funcionario.service.ts
+++ api/src/users/funcionario/funcionario.service.ts
@@ -4,12 +4,14 @@ import { CriarEventoDTO } from "./dto/criarEvento.dto";
 import { UsersService } from "../users.service";
 import { EventoDTO } from "./dto/evento.dto";
 import { EstabelecimentoService } from "src/features/estabelecimento.service";
+import { HistoricoService } from "src/features/historico.service";
 
 @Injectable()
 export class FuncionarioService {
     constructor(private readonly userService: UsersService,
                 private readonly estabelecimentoService: EstabelecimentoService,
-                private readonly eventoService: EventoService) {}
+                private readonly eventoService: EventoService,
+                private readonly historicoService: HistoricoService) {}
 
     async cadastraEvento(data: CriarEventoDTO, userId: number, file: string): Promise<EventoDTO>  {
         const user = await this.userService.getUserById(userId);
@@ -23,5 +25,86 @@ export class FuncionarioService {
         if(!estabelecimento || estabelecimento.Usuario[0].id_estabelecimento != user.id_estabelecimento) throw new HttpException({status: 404, error: 'Estabelecimento não encontrado'}, 404)
         return await this.eventoService.cadastraEvento(data, estabelecimento.id_estabelecimento, Number(process.env.EVENT_STATUS_PENDENTE), file)
         
+    }
+
+    //rota para alterar evento
+    async alteraEvento(userId: number, eventId: number, file: string | null, data: CriarEventoDTO) {
+        const user = await this.userService.getUserById(userId);
+
+        if (!user || !user.id_estabelecimento) {
+            throw new HttpException('Usuário não possui estabelecimento vinculado', 404);
+        } 
+
+        const event = await this.eventoService.buscaEventoPorId(eventId);
+        
+        if (!event || event.id_estabelecimento != user.id_estabelecimento) {
+            throw new HttpException('Evento não encontrado', 404);
+        }
+        
+        const history = await this.historicoService.encontraHistoricoPorEvento(event.id_evento);
+
+        if(event.foto !== file) {
+            const found = history.find(h => h.campo === "foto");
+            if (found) {
+                await this.historicoService.editarHistorico(found.id_his, {valor_novo : file});
+            } else {
+                const payload = {
+                    id_usuario: user.id_usuario,
+                    campo: "foto",
+                    valor_antigo: event.foto,
+                    valor_novo: file,
+                    Evento_Historico: {
+                        create: {
+                            id_evento: event.id_evento
+                        }
+                    }
+                };
+                await this.historicoService.adicionaHistorico(payload);
+            };
+        }
+
+        for (const record of Object.keys(data)) {
+
+            if (!(record in event)) continue;
+
+            const antigo = event[record];
+            const novo = (data as any)[record];
+
+            const antigoStr = antigo instanceof Date ? antigo.toISOString() : String(antigo ?? '');
+            const novoStr = novo instanceof Date ? new Date(novo).toISOString() : String(novo ?? '');
+
+            if (antigoStr === novoStr) continue; // sem alteração -> pula
+
+            const existing = history.find(h => h.campo === record);
+
+            if (existing) {
+
+                if ( existing.valor_novo === novoStr ) continue;
+
+                const payload = {
+                    valor_novo: novoStr,
+                };
+
+                await this.historicoService.editarHistorico(existing.id_his, payload);
+
+            } else {
+
+                const payload = {
+                    id_usuario: user.id_usuario,
+                    campo: record,
+                    valor_antigo: String(event[record]),
+                    valor_novo: novo,
+                    Evento_Historico: {
+                        create: {
+                            id_evento: event.id_evento
+                        }
+                    }
+                };
+                await this.historicoService.adicionaHistorico(payload);
+
+            }
+        }
+
+        return { message: 'Alterações registradas no histórico'};
     }
 }
