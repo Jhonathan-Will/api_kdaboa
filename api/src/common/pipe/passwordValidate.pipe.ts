7fb30f612a2557d5d0cd7fb96cac9449a925822a
Jhonathan William <jhonathanwilliam400@gmail.com>
2025-11-24 20:54:23 -0300

    Feat(Pipe): new Pipe for validate the requirements of the password

---
+++ api/src/common/pipe/passwordValidate.pipe.ts
@@ -0,0 +1,20 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common";
+import { CriarGereneteDTO } from "src/auth/dto/create.dto";
+
+@Injectable()
+export class PasswordValidatePipe implements PipeTransform{
+    transform(user: CriarGereneteDTO, metadata: ArgumentMetadata) {
+        const minLength = 8;
+        const requirements = [/[A-Z]/, /[!@#$%&*+=]/, /[0-9]/, /[a-z]/]
+
+        if(user.senha.length < minLength) throw new Error('Senha não atinge requisitos mínimos');
+
+        for(const requirement of requirements){
+            if(!requirement.test(user.senha)){
+                throw new Error('Senha não atinge requisitos mínimos');
+            }
+        }
+
+        return user;
+    }
+}
